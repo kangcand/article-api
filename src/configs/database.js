f4bfe8e1c17e45d16c5dd8e3b54b5788a234c9f9
candra <candra>
2023-02-05 15:07:39 +0700

    package

---
+++ src/configs/database.js
@@ -0,0 +1,8 @@
+module.exports = {
+    multipleStatements: true,
+    host: 'containers-us-west-99.railway.app',
+    user: 'root',
+    password: 'opPvIV2u3gS6XpTP0dhC',
+    database: 'railway',
+    port: 7679,
+}
